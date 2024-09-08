2c7f5fe392a19dbd334af16e3421778a346c4f55
Shiv Prakash Verma <shivprakashlps@gmail.com>
2024-09-08 18:23:12 +0530

    Initial commit

---
+++ src/test/java/com/shivPrakash/healthCareSystem/HealthCareSystemApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shivPrakash.healthCareSystem;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HealthCareSystemApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
